43a5fcef163f782a0912e13a722bc01941d173ea
D-Gar27 <htetwinkhant999@gmail.com>
2021-12-27 22:55:59 +0630

    please someone help meeee

--- pages/story/[storySlug].jsx
+++ pages/story/[storySlug].jsx
@@ -37,7 +37,7 @@ import { BsArrowLeft } from 'react-icons/bs';
 import Head from 'next/head';
 
 const FullStory = ({ story: storyData }) => {
-  const story = storyData.json();
+  const story = JSON.parse(storyData);
   const router = useRouter();
   const { storySlug } = router.query;
 
